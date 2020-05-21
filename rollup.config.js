4f20ae478ae05b3cd3ddeb96f74e0148e36cdff5
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-05-21 17:06:21 +0200

    remove unnecessary legacy build

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import commonjs from '@rollup/plugin-commonjs'
 import svelte from 'rollup-plugin-svelte'
-import babel from '@rollup/plugin-babel'
 import markdown from '@jackfranklin/rollup-plugin-markdown'
 import glob from 'rollup-plugin-glob'
 import { terser } from 'rollup-plugin-terser'
@@ -11,7 +10,6 @@ import pkg from './package.json'
 
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
-const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
 const onwarn = (warning, onwarn) =>
   (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
@@ -38,30 +36,6 @@ export default {
         dedupe: ['svelte'],
       }),
       commonjs(),
-
-      legacy &&
-        babel({
-          extensions: ['.js', '.mjs', '.html', '.svelte'],
-          runtimeHelpers: true,
-          exclude: ['node_modules/@babel/**'],
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: '> 0.25%, not dead',
-              },
-            ],
-          ],
-          plugins: [
-            '@babel/plugin-syntax-dynamic-import',
-            [
-              '@babel/plugin-transform-runtime',
-              {
-                useESModules: true,
-              },
-            ],
-          ],
-        }),
 
       !dev &&
         terser({
