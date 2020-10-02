fc734221360c679866fa638a11c5e55524a7f7a1
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-10-02 08:25:33 +0200

    🤔

--- src/components/DisqusComments.svelte
+++ src/components/DisqusComments.svelte
@@ -3,7 +3,8 @@
 
   export let id
 
-  $: window &&
+  $: if (process.browser) {
+    window.document.querySelector('#disqus_thread') &&
     window.DISQUS &&
     window.DISQUS.reset({
       reload: true,
@@ -12,12 +13,11 @@
         this.page.identifier = id
       },
     })
+  }
 </script>
 
 <svelte:head>
-  <script src="https://marcelo-blog.disqus.com/embed.js" data-timestamp={+new Date()}>
-
-  </script>
+  <script src="https://marcelo-blog.disqus.com/embed.js" data-timestamp={+new Date()} />
 </svelte:head>
 
 <div id="disqus_thread" />
