03407168cf7c37c86577d6de9143d2878794f104
Marcelo Tokarnia <marcelo.tokarnia@gmail.com>
2022-06-13 09:38:19 -0300

    floripa map

--- src/routes/travel-xp/floripa/index.svelte
+++ src/routes/travel-xp/floripa/index.svelte
@@ -10,89 +10,152 @@
 
 <script>
   import Meta from '../../../components/Meta.svelte'
+  import Map from '../../../components/map/Map.svelte'
+  import Marker from '../../../components/map/Marker.svelte'
+  import InfoWindow from '../../../components/map/InfoWindow.svelte'
+  import BasicInfoWindow from '../../../components/map/BasicInfoWindow.svelte'
+
+  const specs = [
+    {
+      position: { lat: -27.774435, lng: -48.497667 },
+      name: '⭐️⭐️⭐️⭐️⭐️ Lagoinha do Leste',
+      href: '/travel-xp/floripa/lagoinha-do-leste',
+    },
+    {
+      position: { lat: -27.77149999949421, lng: -48.79008554295077 },
+      name: '⭐️⭐️⭐️⭐️⭐️ Cachoeira do Salto do Rio Vermelho',
+      href: '/travel-xp/floripa/cachoeira-rio-vermelho',
+    },
+    {
+      position: { lat: -27.719289873051693, lng: -48.6684983304647 },
+      name: '⭐️⭐️⭐️⭐️⭐️ Cambirela',
+      href: '/travel-xp/floripa/cambirela',
+    },
+    {
+      position: { lat: -27.611051366819726, lng: -48.70504940362288 },
+      name: '⭐️⭐️⭐️⭐️ Pedra Branca',
+      href: '/travel-xp/floripa/pedra-branca',
+    },
+    {
+      position: { lat: -27.584633401280968, lng: -48.42461224132191 },
+      name: '⭐️⭐️⭐️⭐️ Barra da Lagoa -> Piscinas Naturais -> Alto da Boa Vista -> Galheta',
+      href: '/travel-xp/floripa/alto-da-boa-vista',
+    },
+    {
+      position: { lat: -27.575709, lng: -48.471752 },
+      name: '⭐️⭐️⭐️⭐️ Morro do Assopro -> Pedra do Urubu',
+      href: '/travel-xp/floripa/morro-do-assopro',
+    },
+    {
+      position: { lat: -27.472734275332712, lng: -48.37983384209141 },
+      name: '⭐️⭐️⭐️ Santinho -> Morro das Aranha -> Pedra do Marimbondo -> Moçambique',
+      href: '/travel-xp/floripa/morro-das-aranhas',
+    },
+    {
+      position: { lat: -27.446465404516317, lng: -48.36164947755078 },
+      name: '⭐️⭐️⭐️ Ingleses -> Toca da Onça -> Santinho',
+      href: '/travel-xp/floripa/toca-da-onca',
+    },
+    {
+      position: { lat: -27.670401611646646, lng: -48.49290970087412 },
+      name: '⭐️⭐️⭐️ Morro do Lampião',
+      href: '/travel-xp/floripa/morro-do-lampiao',
+    },
+    {
+      position: { lat: -27.74993835301106, lng: -48.53162095819793 },
+      name: '⭐️⭐️⭐️ Trilha da Gurita -> Cachoeiras do Sertão',
+      href: '/travel-xp/floripa/gurita-sertao',
+    },
+    {
+      position: { lat: -27.584633401280968, lng: -48.42461224132191 },
+      name: '⭐️⭐️⭐️ Caverna do Pântano',
+      href: '/travel-xp/floripa/caverna-pantano',
+    },
+    {
+      position: { lat: -27.833720123677498, lng: -48.562555142009835 },
+      name: '⭐️⭐️⭐️ Solidão -> Naufragados -> Caieira',
+      href: '/travel-xp/floripa/naufragados',
+    },
+    {
+      position: { lat: -27.589574601955515, lng: -48.53401406156816 },
+      name: '⭐️⭐️⭐️ Morro da Cruz',
+      href: '/travel-xp/floripa/morro-da-cruz',
+    },
+    {
+      position: { lat: -27.73253323977083, lng: -48.653021091472574 },
+      name: '⭐️⭐️⭐️ Cachoeira do Jarrão',
+      href: '/travel-xp/floripa/cachoeira-jarrao',
+    },
+    {
+      position: { lat: -27.613610896712114, lng: -48.433612429366164 },
+      name: '⭐️⭐️⭐️ Praia do Gravatá -> Secret',
+      href: '/travel-xp/floripa/gravata-secret',
+    },
+    {
+      position: { lat: -27.435810861558277, lng: -48.51695146676019 },
+      name: '⭐️⭐️ Jurerê -> Morro do Farol -> Praia da Daniela',
+      href: '/travel-xp/floripa/morro-do-farol',
+    },
+    {
+      position: { lat: -27.387789238223046, lng: -48.41486199797258 },
+      name: '⭐️⭐️ Lagoinha do Norte -> Morro do Rapa -> Praia Brava',
+      href: '/travel-xp/floripa/morro-do-rapa',
+    },
+    {
+      position: { lat: -27.409829979333622, lng: -48.40739802026528 },
+      name: '⭐️⭐️ Praia Brava -> Morro da Feiticeira -> Ingleses',
+      href: '/travel-xp/floripa/morro-da-feiticeira',
+    },
+    {
+      position: { lat: -27.543693514758985, lng: -48.4639842581842 },
+      name: '⭐️⭐️ Cachoeira da Costa da Lagoa',
+      href: '/travel-xp/floripa/cachoeira-costa-lagoa',
+    },
+    {
+      position: { lat: -27.794418762681993, lng: -48.53767640414582 },
+      name: '⭐️⭐️ Cachoeira da Solidão',
+      href: '/travel-xp/floripa/cachoeira-solidao',
+    },
+    {
+      position: { lat: -27.69632633042641, lng: -48.46591292531046 },
+      name: '⭐️⭐️ Ilha do Campeche',
+      href: '/travel-xp/floripa/ilha-campeche',
+    },
+    {
+      position: { lat: -27.417162162729593, lng: -48.47652271902144 },
+      name: '⭐️⭐️ Ilha do Francês',
+      href: '/travel-xp/floripa/ilha-frances',
+    },
+    {
+      position: { lat: -27.615284643244593, lng: -48.50480521879317 },
+      name: '⭐️⭐️ Cachoeira do Poção',
+      href: '/travel-xp/floripa/cachoeira-pocao',
+    },
+    {
+      position: { lat: -27.569949576576175, lng: -48.537073038250654 },
+      name: '⭐️⭐️ Ponta do Coral',
+      href: '/travel-xp/floripa/ponta-coral',
+    },
+    {
+      position: { lat: -27.38770149927642, lng: -48.43255848960548 },
+      name: '⭐️ Ponta das Canas -> Ponta da Lage -> Lagoinha do Norte',
+      href: '/travel-xp/floripa/ponta-da-lage',
+    },
+  ]
 </script>
 
 <Meta title="Florianópolis" description="Lugarzão" />
 
-<div>Florianópolis</div>
+<h2>Florianópolis</h2>
 
-<div><a href="/travel-xp/floripa/lagoinha-do-leste"> ⭐️⭐️⭐️⭐️⭐️ Lagoinha do Leste </a></div>
+<Map zoom={10} center={spec.position}>
+  {#each specs as spc}
+    <Marker lat={spc.position.lat} lng={spc.position.lng} title={spc.name}>
+      <InfoWindow component={BasicInfoWindow} props={{ title: spc.name, href: spc.href }} />
+    </Marker>
+  {/each}
+</Map>
 
-<div>
-  <a href="/travel-xp/floripa/cachoeira-rio-vermelho">
-    ⭐️⭐️⭐️⭐️⭐️ Cachoeira do Salto do Rio Vermelho
-  </a>
-</div>
-
-<div><a href="/travel-xp/floripa/cambirela"> ⭐️⭐️⭐️⭐️⭐️ Cambirela </a></div>
-
-<div><a href="/travel-xp/floripa/pedra-branca"> ⭐️⭐️⭐️⭐️ Pedra Branca </a></div>
-
-<div>
-  <a href="/travel-xp/floripa/alto-da-boa-vista">
-    ⭐️⭐️⭐️⭐️ Barra da Lagoa -> Piscinas Naturais -> Alto da Boa Vista -> Galheta
-  </a>
-</div>
-
-<div>
-  <a href="/travel-xp/floripa/morro-do-assopro">
-    ⭐️⭐️⭐️⭐️ Morro do Assopro -> Pedra do Urubu
-  </a>
-</div>
-
-<div>
-  <a href="/travel-xp/floripa/morro-das-aranhas">
-    ⭐️⭐️⭐️ Santinho -> Morro das Aranha -> Pedra do Marimbondo -> Moçambique
-  </a>
-</div>
-
-<div>
-  <a href="/travel-xp/floripa/toca-da-onca"> ⭐️⭐️⭐️ Ingleses -> Toca da Onça -> Santinho </a>
-</div>
-<div><a href="/travel-xp/floripa/morro-do-lampiao"> ⭐️⭐️⭐️ Morro do Lampião </a></div>
-<div>
-  <a href="/travel-xp/floripa/gurita-sertao">
-    ⭐️⭐️⭐️ Trilha da Gurita -> Cachoeiras do Sertão
-  </a>
-</div>
-<div><a href="/travel-xp/floripa/caverna-pantano"> ⭐️⭐️⭐️ Caverna do Pântano </a></div>
-<div>
-  <a href="/travel-xp/floripa/naufragados"> ⭐️⭐️⭐️ Solidão -> Naufragados -> Caieira </a>
-</div>
-<div><a href="/travel-xp/floripa/morro-da-cruz"> ⭐️⭐️⭐️ Morro da Cruz </a></div>
-<div><a href="/travel-xp/floripa/cachoeira-jarrao"> ⭐️⭐️⭐️ Cachoeira do Jarrão </a></div>
-<div><a href="/travel-xp/floripa/gravata-secret"> ⭐️⭐️⭐️ Praia do Gravatá -> Secret </a></div>
-
-<div>
-  <a href="/travel-xp/floripa/morro-do-farol">
-    ⭐️⭐️ Jurerê -> Morro do Farol -> Praia da Daniela
-  </a>
-</div>
-
-<div>
-  <a href="/travel-xp/floripa/morro-do-rapa">
-    ⭐️⭐️ Lagoinha do Norte -> Morro do Rapa -> Praia Brava
-  </a>
-</div>
-
-<div>
-  <a href="/travel-xp/floripa/morro-da-feiticeira">
-    ⭐️⭐️ Praia Brava -> Morro da Feiticeira -> Ingleses
-  </a>
-</div>
-
-<div>
-  <a href="/travel-xp/floripa/cachoeira-costa-lagoa"> ⭐️⭐️ Cachoeira da Costa da Lagoa </a>
-</div>
-
-<div><a href="/travel-xp/floripa/cachoeira-solidao"> ⭐️⭐️ Cachoeira da Solidão </a></div>
-<div><a href="/travel-xp/floripa/ilha-campeche"> ⭐️⭐️ Ilha do Campeche </a></div>
-<div><a href="/travel-xp/floripa/ilha-frances"> ⭐️⭐️ Ilha do Francês </a></div>
-<div><a href="/travel-xp/floripa/cachoeira-pocao"> ⭐️⭐️ Cachoeira do Poção </a></div>
-<div><a href="/travel-xp/floripa/ponta-coral"> ⭐️⭐️ Ponta do Coral </a></div>
-
-<div>
-  <a href="/travel-xp/floripa/ponta-da-lage">
-    ⭐️ Ponta das Canas -> Ponta da Lage -> Lagoinha do Norte
-  </a>
-</div>
+{#each specs as spc}
+  <div><a href={spc.href}> {spc.name} </a></div>
+{/each}
--- src/routes/travel-xp/floripa/morro-do-assopro.svelte
+++ src/routes/travel-xp/floripa/morro-do-assopro.svelte
@@ -6,7 +6,7 @@
   import BasicInfoWindow from '../../../components/map/BasicInfoWindow.svelte'
 
   const title = '⭐️⭐️⭐️⭐️ Morro do Assopro'
-  const spc = {"position":{"lat":-27.5757090,"lng":-48.4717520}, name: title }
+  const spc = { position: { lat: -27.575709, lng: -48.471752 }, name: title }
 </script>
 
 <h2>{title}</h2>
@@ -16,17 +16,25 @@
   <Marker lat={spc.position.lat} lng={spc.position.lng} title={spc.name}>
     <InfoWindow component={BasicInfoWindow} props={{ title: spc.name }} />
   </Marker>
-  <Marker lat={-27.5672090} lng={-48.4815460} title={"Pedra do Urubu"}>
-    <InfoWindow component={BasicInfoWindow} props={ {title:"Pedra do Urubu"}} />
+  <Marker lat={-27.567209} lng={-48.481546} title={'Pedra do Urubu'}>
+    <InfoWindow component={BasicInfoWindow} props={{ title: 'Pedra do Urubu' }} />
   </Marker>
 </Map>
 
 <p>
-  <a href="https://www.strava.com/activities/7067009976">Atividade do Strava</a
-  > mostra a primeira saída pra rampa de vôo livre, a segunda pro Mirante do Assopro (com foto) e a terceira é a pedra do Urubu com vista pro continente e shopping Floripa (foto na atividade do strava).
+  <a href="https://www.strava.com/activities/7067009976">Atividade do Strava</a> mostra a primeira saída
+  pra rampa de vôo livre, a segunda pro Mirante do Assopro (com foto) e a terceira é a pedra do Urubu
+  com vista pro continente e shopping Floripa (foto na atividade do strava).
 </p>
-<p>Essa trilha tem umas lendas urbanas ai dum moço que mora lá na Pedra do Urubu que às vezes ameaça a galera com facão 😅 então eu não recomendo ir sozinho</p>
-<p>E realmente, toda vez que eu vou lá eu vejo uma estrutura que realmente parece que alguém mora ali, com cama, fogueira, horta e água, e toda vez está um pouco diferente, dando a impressão que alguém "mantém" aquilo ali. Então cuidado hehehe, não vá sozinho nessa.</p>
+<p>
+  Essa trilha tem umas lendas urbanas ai dum moço que mora lá na Pedra do Urubu que às vezes ameaça
+  a galera com facão 😅 então eu não recomendo ir sozinho
+</p>
+<p>
+  E realmente, toda vez que eu vou lá eu vejo uma estrutura que realmente parece que alguém mora
+  ali, com cama, fogueira, horta e água, e toda vez está um pouco diferente, dando a impressão que
+  alguém "mantém" aquilo ali. Então cuidado hehehe, não vá sozinho nessa.
+</p>
 <p>Distância: 13km</p>
 <p>Elevação: 600m</p>
 
